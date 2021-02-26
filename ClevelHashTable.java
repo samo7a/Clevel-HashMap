2b6c0e7840155db04ce65f84f2f1722a3047ac9a
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2021-02-26 10:36:13 -0500

    insert and resize changes
    my inital changes for insert and resize hashtables, the hashing is not complete yet so I leave out the blank for hashing. Will complete it later today

--- ClevelHashTable.java
+++ ClevelHashTable.java
@@ -20,6 +20,17 @@ public class ClevelHashTable {
 
     public int insert(int value) {
         // TODO
+       // hash here
+
+        // get the key from hashing, assign 0 for now
+        int key = 0;
+        // insert to bottom level
+       bottomLevel[key] = Bucket.insertTree(key, value, bottomLevel[key]);
+
+       // get the key for top level, assign 0 for now
+       int topKey = 0;
+       // insert to top level
+        topLevel[topKey] = Bucket.insertTree(topKey, value, topLevel[topKey]);
         return value;
     }
 
@@ -34,7 +45,27 @@ public class ClevelHashTable {
     }
 
     public void resize() {
-        // TODO
+        // indicate the bucket is resizing
+        isResizing = true;
+        // get the new size for the bottom level
+        // I assume the size would be doubled based on the discord chat
+        size *= 2;
+        Bucket[] newLevel = new Bucket[size];
+        // call hash function here
+
+        // assign the current top level as bottom
+        for(Bucket buc: topLevel) {
+
+        }
+        // could be spicy here since we already assign an initial size to topLevel
+        topLevel = new Bucket[size];
+        //topLevel = newLevel;
+        // assign new Level as the top
+        for(Bucket i : newLevel) {
+
+        }
+        // finish resiziing
+        isResizing = false;
 
     }
 
